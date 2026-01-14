33147dd22cdf905b76863566047ade4915b0f25a
Rogov Alexander <al.rogov@bftcom.com>
2026-01-14 18:25:40 +0300

    initial my js teaching

---
+++ 2.js-basic/00/app.js
@@ -0,0 +1,15 @@
+console.log('papapap123');
+console.log('Возведение в степень');
+console.log(2 === "2");
+console.log(2 == "2");
+
+
+function yourCodeInside(x) {
+  let isOdd;
+  isOdd = x%2;
+
+  return isOdd == 0;
+}
+
+
+console.log(yourCodeInside(112));
---
+++ 2.js-basic/2.6/iftest.js
@@ -0,0 +1,10 @@
+
+function yourCodeInside(x) {
+  let isOdd;
+  isOdd = x%2;
+
+  return isOdd != 0;
+}
+
+
+console.log(yourCodeInside(112));
